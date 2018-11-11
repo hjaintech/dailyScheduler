1273d1cbe9d95fa74fe4988fb029d7709293786d
Harshit Jain <hjain.tech@gmail.com>
2018-11-11 19:46:31 +0530

    Initial Commit

---
+++ app/landing/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+
+
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <Text>This is the first text</Text>
+    );
+  }
+}
